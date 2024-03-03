2bf036c30c58e07b9439a423f8aa363bd15ddcb5
Adarsh Mourya <adarshmourya281@gmail.com>
2024-03-03 13:36:37 +0530

    First Time

---
+++ BootProj06-PatientInformation-LayeredApplication/src/main/java/com/nt/BootProj06PatientInformationLayeredApplication.java
@@ -0,0 +1,14 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class BootProj06PatientInformationLayeredApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=SpringApplication.run(BootProj06PatientInformationLayeredApplication.class, args);
+	}
+
+}
---
+++ BootProj06-PatientInformation-LayeredApplication/src/main/java/com/nt/model/Patient.java
@@ -0,0 +1,8 @@
+package com.nt.model;
+
+public class Patient {
+	
+	private Integer pId;
+	private String pName;
+    private Integer pAmt;
+}
